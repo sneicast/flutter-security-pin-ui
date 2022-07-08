dd5c1a689cb5b20bed5b99bf3f62d2d0c1c68d5b
Sneider Castillo <sneicast@hotmail.com>
2022-07-08 09:11:55 -0500

    ui security pin

--- lib/src/otp_page.dart
+++ lib/src/otp_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class OtpPage extends StatefulWidget {
-  OtpPage({Key? key}) : super(key: key);
+  const OtpPage({Key? key}) : super(key: key);
 
   @override
   State<OtpPage> createState() => _OtpPageState();
@@ -12,7 +12,7 @@ class _OtpPageState extends State<OtpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
               Colors.deepPurpleAccent,
@@ -21,28 +21,336 @@ class _OtpPageState extends State<OtpPage> {
             begin: Alignment.topRight,
           ),
         ),
-        child: OtpScreen(),
+        child: const OtpScreen(),
       ),
     );
   }
 }
 
 class OtpScreen extends StatefulWidget {
-  OtpScreen({Key? key}) : super(key: key);
+  const OtpScreen({Key? key}) : super(key: key);
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
 
+class _OtpScreenState extends State<OtpScreen> {
   List<String> currentPin = ["", "", "", ""];
   TextEditingController pinOneController = TextEditingController();
   TextEditingController pinTwoController = TextEditingController();
   TextEditingController pinThreeController = TextEditingController();
   TextEditingController pinFourController = TextEditingController();
 
-  @override
-  State<OtpScreen> createState() => _OtpScreenState();
-}
+  var outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: const BorderSide(color: Colors.transparent),
+  );
 
-class _OtpScreenState extends State<OtpScreen> {
+  int pinIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Column(
+      children: [
+        buildExitButton(),
+        Expanded(
+          child: Container(
+            alignment: const Alignment(0, 0.5),
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                buildSecurityText(),
+                const SizedBox(
+                  height: 40.0,
+                ),
+                buildPinRow(),
+              ],
+            ),
+          ),
+        ),
+        buildNumberPad(),
+      ],
+    ));
+  }
+
+  buildSecurityText() {
+    return const Text(
+      "Security PIN",
+      style: TextStyle(
+          color: Colors.white70, fontSize: 21.0, fontWeight: FontWeight.bold),
+    );
+  }
+
+  buildNumberPad() {
+    return Expanded(
+      child: Container(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  KeyboardNumber(
+                      n: 1,
+                      onPressed: () {
+                        pinIndexSetup("1");
+                      }),
+                  KeyboardNumber(
+                      n: 2,
+                      onPressed: () {
+                        pinIndexSetup("2");
+                      }),
+                  KeyboardNumber(
+                      n: 3,
+                      onPressed: () {
+                        pinIndexSetup("3");
+                      }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  KeyboardNumber(
+                      n: 4,
+                      onPressed: () {
+                        pinIndexSetup("4");
+                      }),
+                  KeyboardNumber(
+                      n: 5,
+                      onPressed: () {
+                        pinIndexSetup("5");
+                      }),
+                  KeyboardNumber(
+                      n: 6,
+                      onPressed: () {
+                        pinIndexSetup("6");
+                      }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  KeyboardNumber(
+                      n: 7,
+                      onPressed: () {
+                        pinIndexSetup("7");
+                      }),
+                  KeyboardNumber(
+                      n: 8,
+                      onPressed: () {
+                        pinIndexSetup("8");
+                      }),
+                  KeyboardNumber(
+                      n: 9,
+                      onPressed: () {
+                        pinIndexSetup("9");
+                      }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      width: 60.0,
+                      child: const MaterialButton(
+                        onPressed: null,
+                        child: SizedBox(),
+                      )),
+                  KeyboardNumber(
+                      n: 0,
+                      onPressed: () {
+                        pinIndexSetup("0");
+                      }),
+                  Container(
+                      width: 60.0,
+                      child: MaterialButton(
+                        height: 60.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60.0),
+                        ),
+                        onPressed: () {
+                          clearPin();
+                        },
+                        child: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  clearPin() {
+    if (pinIndex == 0) {
+      pinIndex = 0;
+    } else if (pinIndex == 4) {
+      setPin(pinIndex, "");
+      currentPin[pinIndex - 1] = "";
+      pinIndex--;
+    } else {
+      setPin(pinIndex, "");
+      currentPin[pinIndex - 1] = "";
+      pinIndex--;
+    }
+  }
+
+  pinIndexSetup(String text) {
+    if (pinIndex == 0) {
+      pinIndex = 1;
+    } else if (pinIndex < 4) {
+      pinIndex++;
+    }
+    setPin(pinIndex, text);
+    currentPin[pinIndex - 1] = text;
+    String strPin = "";
+    currentPin.forEach((e) {
+      strPin += e;
+    });
+    if (pinIndex == 4) {
+      print(strPin);
+    }
+  }
+
+  setPin(int n, String text) {
+    switch (n) {
+      case 1:
+        pinOneController.text = text;
+        break;
+      case 2:
+        pinTwoController.text = text;
+        break;
+      case 3:
+        pinThreeController.text = text;
+        break;
+      case 4:
+        pinFourController.text = text;
+        break;
+    }
+  }
+
+  buildPinRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        PINNumber(
+          outlineInputBorder: outlineInputBorder,
+          textEditingController: pinOneController,
+        ),
+        PINNumber(
+          outlineInputBorder: outlineInputBorder,
+          textEditingController: pinTwoController,
+        ),
+        PINNumber(
+          outlineInputBorder: outlineInputBorder,
+          textEditingController: pinThreeController,
+        ),
+        PINNumber(
+          outlineInputBorder: outlineInputBorder,
+          textEditingController: pinFourController,
+        ),
+      ],
+    );
+  }
+
+  buildExitButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MaterialButton(
+            onPressed: () {},
+            height: 50.0,
+            minWidth: 50.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0)),
+            child: const Icon(
+              Icons.clear,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PINNumber extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final OutlineInputBorder outlineInputBorder;
+  const PINNumber({
+    Key? key,
+    required this.textEditingController,
+    required this.outlineInputBorder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      child: TextField(
+        controller: textEditingController,
+        enabled: false,
+        obscureText: true,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(16.0),
+          border: outlineInputBorder,
+          filled: true,
+          fillColor: Colors.white30,
+        ),
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 21.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class KeyboardNumber extends StatelessWidget {
+  final int n;
+  final Function() onPressed;
+  const KeyboardNumber({Key? key, required this.n, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60.0,
+      height: 60.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.purpleAccent.withOpacity(0.1),
+      ),
+      alignment: Alignment.center,
+      child: MaterialButton(
+        padding: const EdgeInsets.all(8.0),
+        onPressed: onPressed,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(60.0),
+        ),
+        height: 90.0,
+        child: Text(
+          "$n",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 24 * MediaQuery.of(context).textScaleFactor,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
